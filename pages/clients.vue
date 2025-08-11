c63498a8f84dd98ef2846a4fdccb701dd2a922bd
halloanewahyudi <hallo.anewahyudi@gmail.con>
2025-08-11 09:57:56 +0700

    client

--- pages/clients.vue
+++ pages/clients.vue
@@ -6,19 +6,52 @@
         <h2 class="text-4xl text-primary font-bold">Clients & Strategic Partners</h2>
       </div>
 
+       <div class="mb-10">
+    <h4 class="text-xl mb-5"> Insurance Company </h4>
       <div
         class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 justify-center items-center divide-gray-200 bg-white rounded-xl border border-gray-200"
       >
-        <div v-for="item in clients" class="p-4">
-          <img :src="item?.featured_image" alt="" class="object-contain" />
+        <div v-for="item in ic" class="p-4">
+          <img :src="item?.featured_image" alt="" class="object-contain h-20 mx-auto" />
         </div>
       </div>
+  </div>
+
+
+ <div class="mb-10">
+    <h4 class="text-xl mb-5"> Reinsurance Company </h4>
+      <div
+        class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 justify-center items-center divide-gray-200 bg-white rounded-xl border border-gray-200"
+      >
+        <div v-for="item in rc" class="p-4">
+          <img :src="item?.featured_image" alt="" class="object-contain h-20 mx-auto" />
+        </div>
+      </div>
+  </div>
+      
+  <div class="mb-10">
+    <h4 class="text-xl mb-5"> Joint Brokers </h4>
+      <div
+        class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 justify-center items-center divide-gray-200 bg-white rounded-xl border border-gray-200"
+      >
+        <div v-for="item in jb" class="p-4">
+          <img :src="item?.featured_image" alt="" class="object-contain h-20 mx-auto" />
+        </div>
+      </div>
+  </div>
+   
     </container>
   </div>
 </template>
 
 <script lang="ts" setup>
-const { data: clients, status } = await useApi("/posts/partner?per_page=100");
+
+const { data: ic, status: icStatus } = await useApi("/posts/partner?per_page=100&taxonomy=client-category&term=insurance-company");
+
+const { data: jb, status: jbStatus } = await useApi("/posts/partner?per_page=100&taxonomy=client-category&term=joint-brokers");
+
+const { data: rc, status : rcStatus } = await useApi("/posts/partner?per_page=100&taxonomy=client-category&term=reinsurance-company	");
+
 </script>
 
 <style></style>
