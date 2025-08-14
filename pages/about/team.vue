b223d52e4f3b13233b11f21bc4db6ba0b200f210
halloanewahyudi <hallo.anewahyudi@gmail.con>
2025-08-14 10:24:05 +0700

    team

--- pages/about/team.vue
+++ pages/about/team.vue
@@ -12,7 +12,7 @@
           
            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-max mx-auto justify-center items-center   gap-6 mb-20">
           <div
-            v-for="(item, index) in team"
+            v-for="(item, index) in management"
             :key="item.id"
             @mouseenter="hovered[index] = true"
             @mouseleave="hovered[index] = false"
@@ -36,6 +36,31 @@
           </div>
         </div>
          
+         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-max mx-auto justify-center items-center gap-6 mb-20">
+         <div
+            v-for="(item, index) in staff"
+            :key="item.id"
+            @mouseenter="hovered[index] = true"
+            @mouseleave="hovered[index] = false"
+          >
+            <div
+              class="bg-cover bg-center shrink-0 p-4 w-full max-w-[260px] h-[300px] mx-auto bg-primary rounded-2xl overflow-hidden relative transition-all duration-300 flex flex-col justify-end"
+              :style="{
+                backgroundImage: `url(${
+                  hovered[index] ? item.acf.second_image.url : item.featured_image
+                })`,
+              }"
+            >
+              <div
+                class="absolute inset-0 bg-gradient-to-t from-black/45 via-black/10 to-transparent"
+              ></div>
+              <div class="text-white relative z-10">
+                <h4 class="font-semibold text-white text-sm">{{ item.title }}</h4>
+                <p class="text-xs mb-0 pb-0">{{ item.acf.position }}</p>
+              </div>
+            </div>
+          </div>
+         </div>
        
       </Container>
     </div>
@@ -43,11 +68,10 @@
 </template>
 
 <script setup lang="ts">
-import { useApi } from "~/composables/api";
-import { ref, computed } from "vue";
 
-const { data:team, pending, error } = await useApi("/posts/team?taxonomy=team-category&term=management");
 
+const { data:management, pending, error } = await useApi("/posts/team?taxonomy=team-category&term=management");
+const {data:staff, pending:staffPending, error:staffError} = await useApi("/posts/team?taxonomy=team-category&term=staff");
 
 const hovered = ref<Record<number, boolean>>({});
 
